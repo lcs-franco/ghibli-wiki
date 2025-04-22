d6e9fd4215d81c5d843fc8fa0a5d7d432c04553f
Lucas Franco <lcs.franco556@gmail.com>
2025-04-22 11:36:10 -0300

    feat: get species by id endpoint

---
+++ src/services/species/getById.ts
@@ -0,0 +1,9 @@
+import { Species } from "../../types";
+import { httpClient } from "../httpClient";
+
+type SpeciesResponse = Species;
+
+export async function getById(id: string) {
+  const { data } = await httpClient.get<SpeciesResponse>(`/species/${id}`);
+  return data;
+}
