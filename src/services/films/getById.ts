466bd18a080da83e0586268aed407e5f5729a82b
Lucas Franco <lcs.franco556@gmail.com>
2025-04-22 11:27:23 -0300

    feat: get film by id  endpoint

---
+++ src/services/films/getById.ts
@@ -0,0 +1,9 @@
+import { Film } from "../../types";
+import { httpClient } from "../httpClient";
+
+type FilmResponse = Film;
+
+export async function getById(id: string) {
+  const { data } = await httpClient.get<FilmResponse>(`/films/${id}`);
+  return data;
+}
