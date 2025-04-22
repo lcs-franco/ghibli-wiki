7b6706648dbd45dc83660a326413e421445f9e62
Lucas Franco <lcs.franco556@gmail.com>
2025-04-22 19:54:42 -0300

    feat: vehicles query hook

---
+++ src/lib/hooks/vehicles.ts
@@ -0,0 +1,19 @@
+import { vehiclesService } from "@lib/services/vehicles";
+import { useQuery } from "@tanstack/react-query";
+
+export function useVehicle() {
+  return useQuery({
+    queryKey: ["vehicle"],
+    queryFn: vehiclesService.getAll,
+    staleTime: Infinity,
+  });
+}
+
+export function useVehicleById(id: string) {
+  return useQuery({
+    queryKey: ["vehicle", id],
+    queryFn: () => vehiclesService.getById(id),
+    staleTime: Infinity,
+    enabled: !!id,
+  });
+}
