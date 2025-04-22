6f0a1cbdeb6ab8b089a089521145282230cfdb02
Lucas Franco <lcs.franco556@gmail.com>
2025-04-22 11:30:51 -0300

    feat: get people by id endpoint

---
+++ src/services/peoples/getById.ts
@@ -0,0 +1,9 @@
+import { People } from "../../types";
+import { httpClient } from "../httpClient";
+
+type PeopleResponse = People;
+
+export async function getById(id: string) {
+  const { data } = await httpClient.get<PeopleResponse>(`/people/${id}`);
+  return data;
+}
