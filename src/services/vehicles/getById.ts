f3b1d203eabe9523f9ed248ffa90acdac03d1e5c
Lucas Franco <lcs.franco556@gmail.com>
2025-04-22 11:38:47 -0300

    feat: get vehicles by id endpoint

---
+++ src/services/vehicles/getById.ts
@@ -0,0 +1,9 @@
+import { Vehicles } from "../../types";
+import { httpClient } from "../httpClient";
+
+type VehiclesResponse = Vehicles;
+
+export async function getById(id: string) {
+  const { data } = await httpClient.get<VehiclesResponse>(`/vehicles/${id}`);
+  return data;
+}
