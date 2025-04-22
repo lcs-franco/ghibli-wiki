d6fa1c3ce05cd75f6cb642801e7ac1cef9712b5f
Lucas Franco <lcs.franco556@gmail.com>
2025-04-22 11:35:48 -0300

    feat: get all species endpoint

---
+++ src/services/species/getAll.ts
@@ -0,0 +1,9 @@
+import { Species } from "../../types";
+import { httpClient } from "../httpClient";
+
+type SpeciesResponse = Species[];
+
+export async function getAll() {
+  const { data } = await httpClient.get<SpeciesResponse>("/species");
+  return data;
+}
