059f26185b54c065d09ad6bb4a0529888094e854
Lucas Franco <lcs.franco556@gmail.com>
2025-04-22 11:33:17 -0300

    feat: get location by id endpoint

---
+++ src/services/locations/getById.ts
@@ -0,0 +1,8 @@
+import { httpClient } from "../httpClient";
+
+type LocationResponse = Location;
+
+export async function getById(id: string) {
+  const { data } = await httpClient.get<LocationResponse>(`/locations/${id}`);
+  return data;
+}
