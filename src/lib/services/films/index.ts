abaa2f6ba5d4d4c4d1c2cf84368992bba18867e8
Lucas Franco <lcs.franco556@gmail.com>
2025-05-04 19:11:04 -0300

    feat: film filter type

--- src/lib/services/films/index.ts
+++ src/lib/services/films/index.ts
@@ -1,6 +1,12 @@
 import { getAll } from './getAll'
 import { getById } from './getById'
 
+export type FilmFilters = {
+  director: string | null
+  decade: string | null
+  rating: string | null
+}
+
 export const filmsService = {
   getAll,
   getById,
