17d669608e90754240c1ceaf0da4166c3e8390ce
Lucas Franco <lcs.franco556@gmail.com>
2025-05-12 10:49:57 -0300

    feat: create people filters

--- src/lib/services/peoples/index.ts
+++ src/lib/services/peoples/index.ts
@@ -1,6 +1,11 @@
 import { getAll } from './getAll'
 import { getById } from './getById'
 
+export type PeopleFilters = {
+  species: string | null
+  gender: string | null
+}
+
 export const peoplesService = {
   getAll,
   getById,
